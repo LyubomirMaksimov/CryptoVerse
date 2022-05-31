8f4af08ed5d342eb251c46df03a25fbee105158f
lyubomirmaksimov <lyubomir.maksimov@gmail.com>
2022-05-31 10:22:36 +0300

    Finished with Navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import "antd/dist/antd.css";
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//   document.getElementById("root")
+// );
+
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
